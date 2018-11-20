c62ff0b99366a8956d82f24d2a9517d52ab6428b
zurilawrence <zlawrence@gwmail.gwu.edu>
2018-11-20 16:48:07 -0500

    Add Firebase dependencies

--- Rolecall/app/src/main/java/com/example/owner/rolecall/ui/LoginActivity.kt
+++ Rolecall/app/src/main/java/com/example/owner/rolecall/ui/LoginActivity.kt
@@ -12,13 +12,14 @@ import android.support.v7.app.AlertDialog
 import android.view.View
 import android.widget.*
 
-import com.crashlytics.android.Crashlytics
+/*import com.crashlytics.android.Crashlytics
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-//import com.google.android.gms.maps.CameraUpdateFactory
-//import com.google.android.gms.maps.model.LatLng
-//import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+*/
 import java.util.*
 
 class LoginActivity : AppCompatActivity() {
