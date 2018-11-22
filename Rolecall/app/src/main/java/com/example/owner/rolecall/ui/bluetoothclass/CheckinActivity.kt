5c4b3d032ac2a53a2e83a8c5342dfbc1ee342b98
zurilawrence <zlawrence@gwmail.gwu.edu>
2018-11-22 02:00:34 -0500

    List bluetooth connections

---
+++ Rolecall/app/src/main/java/com/example/owner/rolecall/ui/bluetoothclass/CheckinActivity.kt
@@ -0,0 +1,36 @@
+package com.example.owner.rolecall.ui.bluetoothclass
+
+import com.example.owner.rolecall.R
+import android.content.Context
+import android.content.Intent
+import android.location.Address
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.support.v7.app.AlertDialog
+import android.view.View
+import android.widget.*
+
+import com.crashlytics.android.Crashlytics
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+
+import java.util.*
+
+class CheckinActivity: AppCompatActivity() {
+
+    companion object {
+        // ... because we want this constant to be shared with another class
+        val CRN = "CRN"
+        val USER = "USERNAME"
+
+    }
+
+    /*
+     * TODO:
+     * Student - Search for available Nearby bluetooth signal
+     * Teacher - Start hub for bluetooth signin
+     */
+}
